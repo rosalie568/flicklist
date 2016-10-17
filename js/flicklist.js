23808084736ef1e40a549287c09c564401d0b888
Tiffany <rosalie568@hotmail.com>
2016-10-17 17:12:36 -0500

    Added work to html page

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,5 +1,4 @@
 
-
 var model = {
   watchlistItems: [],
   browseItems: []
@@ -26,15 +25,15 @@ function discoverMovies(callback) {
 		success: function(response) {
 			console.log("We got a response from The Movie DB!");
 			console.log(response);
-			
+
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
-			// invoke the callback function that was passed in. 
+
+			// invoke the callback function that was passed in.
 			callback();
 		}
 	});
-  
+
 }
 
 
@@ -44,22 +43,22 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
-  
+
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
-  // for each movie on the current browse list, 
+
+  // for each movie on the current browse list,
   model.browseItems.forEach(function(movie) {
 		// TODO 3
 		// insert a list item into the <ul> in the browse section
-		
+
 		// TODO 4
 		// the list item should include a button that says "Add to Watchlist"
-		
+
 		// TODO 5
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
   });
-  
+
 }
 
 
@@ -68,4 +67,3 @@ function render() {
 $(document).ready(function() {
   discoverMovies(render);
 });
-
